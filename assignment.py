5d2cd1e2e950132a2c0b4ce4d1974c0f217026f8
joanna.wojcik <joanna.wojcik@hotmail.com>
2018-10-30 21:25:34 +0000

    Added pandas data reading

--- assignment.py
+++ assignment.py
@@ -1,12 +1,22 @@
 import numpy as np
+import pandas as pd
 
 
 class Assignment:
+    path_to_cancer = './dataset/cancer'
+    path_to_cancer_training = path_to_cancer + '/trainingData2.csv'
+    cancer_dataset_column_headers = ['bi_rads', 'age', 'shape', 'margin', 'density', 'severity']
 
     def calculate_distance(self, a, b):
-        dist = np.linalg.norm(a-b)
+        dist = np.linalg.norm(a - b)
         return dist
+
+    def load_data(self):
+        df = pd.read_csv(self.path_to_cancer_training,
+                         names=self.cancer_dataset_column_headers, header=None)
+        print(df.head())
 
 
 if __name__ == '__main__':
-    pass
+    subject = Assignment()
+    subject.load_data()
--- test_assignment.py
+++ test_assignment.py
@@ -10,6 +10,12 @@ class TestAssignment(unittest.TestCase):
         print('test value is {0}'.format(result))
         self.assertEqual(result, 4)
 
+    def test_calculate_distance_when_start_larger_than_end_then_distance_is_calculated(self):
+        under_test = Assignment()
+        result = under_test.calculate_distance(10, 6)
+        print('test value is {0}'.format(result))
+        self.assertEqual(result, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
