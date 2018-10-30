40c1200ff267d31d337e8d54a71639a50ff20ced
joanna.wojcik <joanna.wojcik@hotmail.com>
2018-10-30 21:40:19 +0000

    loading data parametrized. Started main flow func

--- assignment.py
+++ assignment.py
@@ -7,16 +7,24 @@ class Assignment:
     path_to_cancer_training = path_to_cancer + '/trainingData2.csv'
     cancer_dataset_column_headers = ['bi_rads', 'age', 'shape', 'margin', 'density', 'severity']
 
-    def calculate_distance(self, a, b):
+    def calculate_distances(self, a, b):
         dist = np.linalg.norm(a - b)
         return dist
 
-    def load_data(self):
-        df = pd.read_csv(self.path_to_cancer_training,
-                         names=self.cancer_dataset_column_headers, header=None)
+    def load_data(self, path):
+        df = pd.read_csv(path, names=self.cancer_dataset_column_headers, header=None)
         print(df.head())
+        return df
+
+    def main(self):
+        # first need to load the training dataset
+        df = self.load_data(self.path_to_cancer_training)
+
+        #converting pandas dataframe to numpy array
+        numpyArray = df.values
+        #distances = self.calculate_distances(numpyArray) -> need to refactor calc dist from basic to one taking in above array
 
 
 if __name__ == '__main__':
     subject = Assignment()
-    subject.load_data()
+    subject.main()
